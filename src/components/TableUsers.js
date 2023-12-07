2aff52e4127b57d29ae70320209dfdca924c9020
minhluquang <luminh2004@gmail.com>
2023-12-07 15:22:54 +0700

    4. Hiện thị list users (sử dụng axios)

--- src/components/TableUsers.js
+++ src/components/TableUsers.js
@@ -1,9 +1,51 @@
-import React from 'react'
+import { useEffect, useState } from "react";
+import Table from "react-bootstrap/Table";
+
+import { fetchAllUser } from "../services/UserService";
 
 const TableUsers = () => {
-  return (
-    <div>TableUsers</div>
-  )
-}
+  const [userList, setUserList] = useState([]);
 
-export default TableUsers
+  useEffect(() => {
+    // Call apis
+    getUsers();
+  }, []);
+
+  const getUsers = async () => {
+    const res = await fetchAllUser();
+    if (res && res.data && res.data.data) {
+      setUserList(res.data.data);
+    }
+  };
+
+  return (
+    <div>
+      <Table striped bordered hover variant="white">
+        <thead>
+          <tr>
+            <th>ID</th>
+            <th>Email</th>
+            <th>First name</th>
+            <th>Last name</th>
+          </tr>
+        </thead>
+        <tbody>
+          {userList &&
+            userList.length > 0 &&
+            userList.map((item, index) => {
+              return (
+                <tr key={`users-${index}`}>
+                  <td>{item.id}</td>
+                  <td>{item.email}</td>
+                  <td>{item.first_name}</td>
+                  <td>{item.last_name}</td>
+                </tr>
+              );
+            })}
+        </tbody>
+      </Table>
+    </div>
+  );
+};
+
+export default TableUsers;
